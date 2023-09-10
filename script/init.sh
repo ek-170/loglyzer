7aae65add09562547137b4f0739926114caaf7ca
Takahiro Muramoto <muramoto@recomot.co.jp>
2023-09-10 22:50:02 +0900

    add init script

--- script/init.sh
+++ script/init.sh
@@ -1,3 +1,30 @@
-#!/bin/bash
+#!/bin/sh
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+data_dir="$script_dir/init_data"
+es_dir="$data_dir/es"
+ingest_dir="$es_dir/ingest"
+elasticsearch_url="http://localhost:9200"
 
-# Assume that this called by docker when initialize application
+set -eu
+
+printf '\e[33mInitialize Elasticsearch\e[m\n'
+printf 'init start Ingest Pipeline\n'
+
+for json_file in "$ingest_dir"/*.json; do
+    if [ -e "$json_file" ]; then
+        filename=$(basename "$json_file")
+        # replace - to /
+        path="${filename//-//}"
+        # remove extension
+        path="${path//.json//}"
+        # remove last /
+        path="${path/%?/}"
+
+        curl_command="curl -XPUT -s \"$elasticsearch_url/$path\" -H \"Content-Type: application/json\" -d @$json_file"
+        echo "Executing: $curl_command"
+        eval "$curl_command" | jq
+        printf "Completed: %s\n" "$filename"
+    fi
+done
+
+printf "\e[33mElasticsearch Initialization completed.\e[m\n"
